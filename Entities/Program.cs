e37abed89907ece46d9c10f0589bf1f47da490c6
David Donelan <141905121+DavidDonelan@users.noreply.github.com>
2026-02-26 23:24:34 +0100

    Modify Library.Go call to include output folder
    Updated the call to Library.Go to include an output folder parameter and added error handling message.

--- Entities/Program.cs
+++ Entities/Program.cs
@@ -1,12 +1,20 @@
-﻿using PicoGK;
+//using Leap71.ShapeKernel; //if available
+using PicoGK;
 using Entities;
 
 
+//string strOutputFolder = ""; //use whatever folder
+
 try
 {
-    Library.Go(0.5f, Entities.MeshSurf.Run);
+    PicoGK.Library.Go(
+        0.5f,
+        Entities.MeshSurf.Run,
+        strOutputFolder
+        );
 }
 catch (Exception e)
 {
+    Console.WriteLine("Failed to run Task.");
     Console.WriteLine(e.ToString());
 }
